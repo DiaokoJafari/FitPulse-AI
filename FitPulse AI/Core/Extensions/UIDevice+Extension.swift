fb6cfaca1b746d846621bb962395ae737fb38c98
DiaokoJafari <diaokojafari@gmail.com>
2025-11-27 11:53:54 +0000

    feat(platform): add UIDevice extension for iPad/iPhone detection

---
+++ FitPulse AI/Core/Extensions/UIDevice+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIDevice+Extension.swift
+//  FitPulse AI
+//
+//  Created by Diaoko Jafari on 26/11/2025.
+//
+
+import UIKit
+
+extension UIDevice {
+    static var isIpad: Bool { UIDevice.current.userInterfaceIdiom == .pad }
+    static var isIphone: Bool { UIDevice.current.userInterfaceIdiom == .phone }
+}
