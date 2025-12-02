e03b5f69a527e04b6bd72d191a8a1782163c9842
Diaoko_Jafari <diaokojafari@gmail.com>
2025-12-02 21:23:24 +0000

    feat(splash): refactor splash view for automatic sizing on iPad and iPhone

--- FitPulse AI/Core/Component/LottieSplashView.swift
+++ FitPulse AI/Core/Component/LottieSplashView.swift
@@ -15,27 +15,34 @@ struct LottieSplashView: View {
     @State private var opacity: Double = 1.0
     
     var body: some View {
-        ZStack {
-            Color.white
-                .ignoresSafeArea()
-            
-            LottieView(animation: .named("ExerciseForHealth"))
-                .playing(loopMode: .playOnce)
-                .configure { animationView in
-                    animationView.play { completed in
-                        if completed {
-                            withAnimation {
-                                opacity = 0.0
-                            }
-                            
-                            Task {
-                                try? await Task.sleep(for: .seconds(0.2))
-                                isShowing = false
+        GeometryReader { geometry in
+            ZStack {
+                Color.white
+                    .ignoresSafeArea()
+                
+                LottieView(animation: .named("ExerciseForHealth"))
+                    .playing(loopMode: .playOnce)
+                    .configure { animationView in
+                        animationView.play { completed in
+                            if completed {
+                                withAnimation {
+                                    opacity = 0.0
+                                }
+                                
+                                Task {
+                                    try? await Task.sleep(for: .seconds(0.2))
+                                    isShowing = false
+                                }
                             }
                         }
                     }
-                }
-                .opacity(opacity)
+                    .opacity(opacity)
+                    .frame(
+                        width: geometry.size.width / 3,
+                        height: geometry.size.width / 3
+                    )
+                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+            }
         }
     }
 }
