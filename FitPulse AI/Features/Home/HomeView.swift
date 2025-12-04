91ab9fb937f61bb92dd230b49a0cbdbeb78f16fd
Diaoko_Jafari <diaokojafari@gmail.com>
2025-12-04 23:02:14 +0000

    Refactor (HomeView): rename variable, fix alignment, add gradient

--- FitPulse AI/Features/Home/HomeView.swift
+++ FitPulse AI/Features/Home/HomeView.swift
@@ -11,13 +11,42 @@ struct HomeView: View {
     @StateObject private var viewModel = DIContainer.shared.makeHomeViewModel()
     
     var body: some View {
-        VStack {
-            Text(viewModel.greeting)
-                .font(.largeTitle)
-            Text("\(viewModel.todayCalories) کالری امروز")
+        VStack(spacing: 30) {
+            VStack(alignment: .leading, spacing: 2) {
+                Text(viewModel.greeting)
+                    .font(.appTitle)
+                Text(viewModel.suggestionMessage)
+                    .font(.appBody)
+                    .foregroundStyle(.black.opacity(0.6))
+            }
+            .multilineTextAlignment(.leading)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal)
+            
+            GradientRectangleView()
+            
+            Spacer()
         }
+        
         .task {
             viewModel.loadData()
         }
+    }
+}
+
+struct GradientRectangleView: View {
+    var body: some View {
+        let startColor = Color(red: 100/255, green: 100/255, blue: 232/255)
+        let endColor = Color(red: 135/255, green: 60/255, blue: 226/255)
+
+        Rectangle()
+            .fill(.linearGradient(
+                colors: [startColor, endColor],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            ))
+            .frame(height: 200)
+            .cornerRadius(20)
+            .padding(.horizontal)
     }
 }
--- FitPulse AI/Features/Home/HomeViewModel.swift
+++ FitPulse AI/Features/Home/HomeViewModel.swift
@@ -9,10 +9,31 @@ import Foundation
 
 @MainActor
 final class HomeViewModel: ObservableObject {
-    @Published var greeting: String = "سلام!"
+    @Published var greeting: String = ""
+    @Published var suggestionMessage: String = "Ready for today's workout?"
     @Published var todayCalories: Int = 0
     
+    init() {
+        updateGreeting()
+    }
+    
     func loadData() {
-        todayCalories = 450 // mock
+        todayCalories = 450
+    }
+    
+    func updateGreeting() {
+        let hour = Calendar.current.component(.hour, from: Date())
+        
+        switch hour {
+        case 5..<12:
+            greeting = "Good Morning!"
+        case 12..<17:
+            greeting = "Good Afternoon!"
+        case 17..<22:
+            greeting = "Good Evening!"
+        default:
+            greeting = "It’s Pretty Late!"
+            suggestionMessage = "I think tomorrow works better."
+        }
     }
 }
